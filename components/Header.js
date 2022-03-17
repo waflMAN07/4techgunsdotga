b06cdde6d39c7bbe2c356ccc032f50b94a2854ec
waflMAN07 <100540234+waflMAN07@users.noreply.github.com>
2022-03-17 14:48:33 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -7,11 +7,17 @@ export default function Header() {
     <div>
       <Link href="/" ><a className={styles.defaultLinkButton}>&#xf7db; Home</a></Link>
       |
-      <Link href="/about"><a className={styles.defaultLinkButton}>&#xf7fb; About</a></Link>
+      <Link href="/games"><a className={styles.defaultLinkButton}>&#xf7fb; Games</a></Link>
       |
-      <Link href="/pyseries"><a className={styles.defaultLinkButton}>&#xf11b; The Py Series</a></Link>
+      <Link href="/avatar"><a className={styles.defaultLinkButton}>&#xf11b; Avatar</a></Link>
       |
-      <Link href="/avatar"><a className={styles.defaultLinkButton}>Avatar</a></Link>
+      <Link href="/create"><a className={styles.defaultLinkButton}>Create</a></Link>
+      |
+      <Link href="/users"><a className={styles.defaultLinkButton}>Users</a></Link>
+      |
+      <Link href="https://discord.com"><a className={styles.defaultLinkButton}>Discord</a></Link>
+      |
+      <Link href="index.html"><a className={styles.defaultLinkButton}>Log Out</a></Link>
       <hr />
     </div>
   )
