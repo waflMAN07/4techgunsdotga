98106591ad88c4e2306f4c6bde40d6660f0255fc
4techguns <4techgunsalt@gmail.com>
2022-03-17 20:44:12 -0230

    Fix styles

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <div>
-<p style={{textAlign: "center"}}>test</p>
-<style jsx>
-body {
-  background-color: #000000;
-}
-</style>
+      <p style={{textAlign: "center"}}>test</p>
+      <style jsx global>{`
+        body {
+          background-color:#aa000a;
+        }
+      `}</style>
     </div>
   );
 }
