f81b8a86f69adeb3d364e3f6da273021be52bcf4
waflMAN07 <100540234+waflMAN07@users.noreply.github.com>
2022-03-18 08:33:47 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -6,13 +6,8 @@ export default function Footer()
         <div className={styles.defaultText}>
             <hr />
             <p>Copyright &copy; Bluen&apos;t 2022. All rights not reserved lmao you think we have lawyers for a personal site and a non-registered company i do not care what you do go steal the code on my for all i care</p>
-            <a href="a.png">Built with KarbML 6&trade;</a>
+            <center><a href="a.png">Built with KarbML 6&trade;</a></center>
             <p className={styles.comiksand}>POWERED BY KARBIS STUDIO BACKPAGE</p>
             <p className={styles.comiksand}>ALSO POWERED BY PAKISTAN SERVER IN LAHORE WITH 66954MS PING</p>
-a{
-  display: block;
-  text-align: center;
-}
-<center><a href="#">My link</a></center>
     )
 }
