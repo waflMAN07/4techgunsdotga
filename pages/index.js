44161eccd532e6e413e601888e030c77c629fa13
waflMAN07 <100540234+waflMAN07@users.noreply.github.com>
2022-03-18 07:57:20 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <p style={{textAlign: "center"}}>test</p>
+      <p style={{textAlign: "center"}}>Where coming soon, sorry where still building the css</p>
       <style jsx global>{`
         body {
           background-color:#aa000a;
