9166b5d67127c406655076310ce91ddd15affe9e
4techguns <42378704+4techguns@users.noreply.github.com>
2022-03-21 08:16:55 -0230

    Ok so uhhhhhhh this miiiight fix it 👍

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      <p style={{textAlign: "center"}}>Where coming soon, sorry where still building the css</p>
+    <>
+      <Head>
+        <title>test</title>
+      </Head>
+      <div>
+        <h1 style={{textAlign: "center"}}>This is a log out test</h1>
+        <p style={{textAlign: "center"}}>Where coming soon, sorry where still building the css</p>
+      </div>
       <style jsx global>{`
         body {
           background-color:#aa000a;
         }
       `}</style>
-    </div>
+    </>
   );
 }
