0e7491d958e983e93597fb62896bc98668ceacc9
4techguns <42378704+4techguns@users.noreply.github.com>
2022-03-21 08:31:29 -0230

    Add image

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
         <title>test</title>
       </Head>
       <div>
+        <Image src="cb-logo.png" width="1181" height="179"></Image>
         <h1 style={{textAlign: "center"}}>This is a log out test</h1>
         <p style={{textAlign: "center"}}>Where coming soon, sorry where still building the css</p>
       </div>
